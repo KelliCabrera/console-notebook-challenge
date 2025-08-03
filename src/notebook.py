6c35c14829df044ed78a5a284a398a31a00d3ea5
Jesus Hincapie <jahlon@outlook.com>
2025-08-03 18:11:25 -0500

    edit 79a2aad Update README to reflect correct file paths and enhance AI usage advice

--- src/notebook.py
+++ src/notebook.py
@@ -1,1 +1,29 @@
 # TODO: Agrega el código de las clases del modelo aquí. Borra este comentario al terminar.
+from datetime import datetime
+
+class Note:
+    HIGH: str = "HIGH"
+    MEDIUM: str = "MEDIUM"
+    LOW: str = "LOW"
+
+    def __init__(self, code: int, title: str, text: str, importance: str):
+        self.code : str = code
+        self.title : str = title
+        self.text: str = text
+        self.importance : str = importance
+        self.creation_date : str = datetime.now()
+        self.tags: list[str] = []
+
+
+
+
+
+
+
+
+
+
+
+
+
+
